d6902b8244477bda455e63b257f81768a8560a1f
whyNat <nwaszak@wp.pl>
2020-04-11 22:20:20 +0200

    Init first project stage

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+
+
+namespace GradeBook
+{
+  class Book
+  {
+    public Book(string name) 
+    {
+      grades = new List<double>();
+      this.name = name;
+    }
+
+    public void AddGrade(double grade)
+    {
+        grades.Add(grade);
+    }
+
+    public void ShowStatistics()
+    {
+      var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+
+            foreach (var number in grades)
+            {
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade);
+                result += number;
+            }
+            result /= grades.Count();
+            Console.WriteLine($"The lowest grade is {lowGrade}");
+            Console.WriteLine($"The highest grade is {highGrade}");
+            Console.WriteLine($"The average grade is {result:N1}");
+    }
+
+    private List<double> grades;
+    private string name;
+  } 
+}
---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            var book = new Book("My Grade Book");
+            book.AddGrade(33.33);
+            book.ShowStatistics();  
+        }
+    }
+}
